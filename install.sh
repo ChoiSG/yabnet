d1fa5c97dbc4c93d62e456e19056a60986e0b34f
Sunggwan Choi <sxc2679@rit.edu>
2020-01-11 02:25:40 -0500

    fix install

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 # Install required packages - Pandoc is required for staticx 
 apt-get install -y python3-pip
 sudo pip3 install --upgrade pip
+apt-get install -y patchelf
 apt-get install -y pandoc
 pip3 install -r requirements.txt
